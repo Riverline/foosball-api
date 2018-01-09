55ab79d7c2f8fe5f2ab80f144c55c76257433c99
Sebastien Porati <sebastien@webrivage.com>
2018-01-09 10:01:20 +0100

    remove js compiled files

--- tests/unit/PlayersController.test.ts
+++ tests/unit/PlayersController.test.ts
@@ -1,7 +1,7 @@
 import { Test } from '@nestjs/testing';
-import {PlayersController} from "../src/API/Application/Controllers/PlayersController";
-import {playerProviders} from "../src/API/Infrastructure/Persistence/TypeORM/PlayerProviders";
-import {DatabaseModule} from "../src/API/Application/Modules/DatabaseModule";
+import {PlayersController} from "../../src/API/Application/Controllers/PlayersController";
+import {playerProviders} from "../../src/API/Infrastructure/Persistence/TypeORM/PlayerProviders";
+import {DatabaseModule} from "../../src/API/Application/Modules/DatabaseModule";
 
 const mochaPlugin = require("serverless-mocha-plugin");
 const expect = mochaPlugin.chai.expect;
@@ -15,15 +15,12 @@ describe('PlayersController', () => {
             controllers: [PlayersController],
             components: [...playerProviders],
         }).compile();
+
         playersController = module.get<PlayersController>(PlayersController);
     });
 
     describe('findAll', () => {
-        it('should return an array of cats', async () => {
-            // const result = ['test'];
-            // jest.spyOn(playerRepository, 'find').mockImplementation(() => result);
-
-            // expect(await playersController.list()).toBe(result);
+        it('should return an array of players', async () => {
             expect(await playersController.list()).to.not.be.empty;
         });
     });
